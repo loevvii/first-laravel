129a56157d55468964a4827a4b72a60c4b77375c
loevvii <lhvinas112203@gmail.com>
2026-02-28 17:49:47 +0800

    Softdeletes

--- app/Models/StorageLocation.php
+++ app/Models/StorageLocation.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StorageLocation extends Model
 {
-    /** @use HasFactory<\Database\Factories\StorageLocationFactory> */
+    use SoftDeletes;
     use HasFactory;
         protected $fillable = [
         'name',
